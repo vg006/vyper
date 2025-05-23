41c91d1ecc54d6d616deffe988c5b9ce817d6dcb
vg006 <devvg006@gmail.com>
2025-05-23 19:25:01 +0530

    🔧 refac: Managed "cli" package utilities
    Removed:
     - isRoot()
     - printArgs()

--- internal/cli/utils.go
+++ internal/cli/utils.go
@@ -1,20 +1,8 @@
 package cli
 
 import (
-	"flag"
 	"strings"
 )
-
-func isRoot() bool {
-	if len(c.args) == 0 {
-		return true
-	}
-	flag.Parse()
-	if len(flag.Args()) == 0 {
-		return true
-	}
-	return false
-}
 
 func isExistingCommand(name string) bool {
 	_, exists := c.commands[name]
@@ -26,10 +14,4 @@ func isFlag(arg string) bool {
 		return false
 	}
 	return true
-}
-
-func printArgs(args []string) {
-	for _, arg := range args {
-		println(arg)
-	}
 }
